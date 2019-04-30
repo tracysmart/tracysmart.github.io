77676e567de483ce7d7756853d426ed4b0fc7573
Tracy Smart <tracysmart@Tracys-MacBook-Pro.local>
2019-04-29 20:50:12 -0400

    Portfolio site first save

---
+++ index.js
@@ -0,0 +1,12 @@
+/* my added fade-in)
+function fadeIn()
+{
+   $(this).fadeIn( fadeOut );
+}
+
+function fadeOut()
+{
+   $(this).fadeOut( fadeIn );
+}
+
+fadeIn.call($("#BrainIllustration.png"));*/
